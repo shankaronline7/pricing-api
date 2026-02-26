ee0bdc7bcc3d59850d5701aba0cf3a09bb85e571
shankar pinjarla <shankarpinjarla6@gmail.com>
2026-02-26 21:40:32 +0530

    pagination

---
+++ Application/DTOs/BasePriceLeasingPagedDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class BasePriceLeasingPagedDto
+    {
+        public string BrandName { get; set; }
+        public string Series { get; set; }
+        public string ModelRange { get; set; }
+        public string ModelCode { get; set; }
+        public string ModelDescription { get; set; }
+        public double ModelBasePrice { get; set; }
+    }
+}
